ccaec69c45af0a919d0dfb4a11baa3896a5c8d41
tidesjunior2018 <aristidesadj@gmail.com>
2023-07-09 17:57:56 -0300

    Update jogodaforca.py

--- jogodaforca.py
+++ jogodaforca.py
@@ -6,6 +6,28 @@ Faça um jogo da forca.
 lista=['GARRAFA','PRATELEIRA','PAREDE','CALENDÁRIO','GATO','CACHORRO']
 chances=6
 tentativas=0
-estadoinicial=["_"]*len(lista)
 palavra=random.choice(lista)
+letrasescolhidas=[]
+estadoinicial=["_"]*len(palavra)
+print(palavra)
 print(estadoinicial)
+
+while(tentativas<chances):
+    letra=input('Digite uma letra: ').upper()
+    letrasescolhidas.append(letra)
+    
+    if letra in palavra:            
+        print(f'VOCÊ ACERTOU A LETRA {letra}') 
+        estadoinicial=letra
+    else:
+        print(f'VOCÊ ERROU A LETRA {letra}')
+        tentativas+=1
+    print(f'Chances = {chances}')
+    print(f'Tentativas = {tentativas}')
+    print
+    print(estadoinicial)
+
+#print('=',*30)
+print('Fim do programa!!!')
+#print('=',*30)
+parei o video 22 min
